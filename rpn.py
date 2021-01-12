abfecba1c4fd08d8178fce3e256f4bc1f9d739f2
FuryWingsYT <32018052+FuryWingsYT@users.noreply.github.com>
2021-01-12 15:00:19 -0500

    Create rpn.py

---
+++ rpn.py
@@ -0,0 +1,36 @@
+stack = []
+gamingGamer = True
+def isnumber(s):
+    try:
+        n = float(s)
+        return True
+    except ValueError:
+        return False
+
+while gamingGamer == True:
+    inp_str = input("Something something prompt something something")
+    someinput = inp_str.split( )
+    for i in someinput:
+        if isnumber(i):
+            stack.append(float(i))
+        else:
+            if i == "+":
+                jeemo = stack.pop()
+                meemo = stack.pop()
+                print(str(meemo+jeemo))
+            if i == "-":
+                jeemo = stack.pop()
+                meemo = stack.pop()
+                print(str(meemo-jeemo))
+            if i == "*":
+                jeemo = stack.pop()
+                meemo = stack.pop()
+                print(str(meemo*jeemo))
+            if i == "/":
+                jeemo = stack.pop()
+                meemo = stack.pop()
+                print(str(meemo/jeemo))
+            if i == "p": print(stack)
+            if i == "i": stack.append(1/stack.pop())
+            if i == "n": stack.append(-1*stack.pop())
+            if i == "x": gamingGamer = False
